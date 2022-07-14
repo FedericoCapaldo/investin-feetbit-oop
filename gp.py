08b9b6cf08b0fc259239b61fe3c13aa98dc66b7f
Federico Capaldo <federico.capaldo1@gmail.com>
2022-07-14 20:33:31 +0100

    added Doctor class to GP

--- gp.py
+++ gp.py
@@ -7,11 +7,27 @@ class Patient:
     def get_name(self):
         return self.first_name + " " + self.last_name
 
+class Doctor:
+
+    def __init__(self, fname, lname, s):
+        self.first_name = fname
+        self.last_name = lname
+        self.specialization = s
+
+    def get_name(self):
+        return self.first_name + " " + self.last_name
+
+    def get_specialization(self):
+        return self.specialization
+
 
 if __name__ == "__main__":
 
     patient1 = Patient("federico", "capaldo")
     patient2 = Patient("jane", "smith")
+    doctor1 = Doctor("Alan", "Pollock", "Pediatrician")
+    doctor2 = Doctor("Jennifer", "Finger", "Hand specialist")
 
     print(patient1.get_name())
     print(patient2.get_name())
+    print(doctor1.get_name(), doctor1.get_specialization())
